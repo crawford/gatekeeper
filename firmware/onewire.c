1bd825cf5ad4e2dbe8ed0020ff53e8b3d81dfe0c
Alex Crawford <adrofwarc@gmail.com>
2011-09-03 13:00:37 -0400

    Removed search and write related functions

--- firmware/onewire.c
+++ firmware/onewire.c
@@ -1,8 +1,8 @@
 /*
    Copyright (c) 2007, Jim Studt
 
-   Removed arduino specific calls, slimmed down codebase, and did some
-   formatting -- Alex Crawford, Sept 3, 2011
+   Removed arduino specific calls, slimmed down codebase, removed all search
+   and write functions, and did some formatting -- Alex Crawford, Sept 3, 2011
 
    Updated to work with arduino-0008 and to include skip(OneWire* data) as of
    2007/07/06. --RJL20
@@ -78,58 +78,6 @@ void initOneWire(uint8_t pinArg, OneWire* data) {
 }
 
 //
-// Perform the onewire reset function.  We will wait up to 250uS for
-// the bus to come high, if it doesn't then it is broken or shorted
-// and we return a 0;
-//
-// Returns 1 if a device asserted a presence pulse, 0 otherwise.
-//
-uint8_t reset(OneWire* data) {
-	uint8_t r;
-	uint8_t retries;
-	retries= 125;
-
-	// wait until the wire is high... just in case
-	SET_MODE(data->modeReg, data->pin, INPUT);
-	do {
-		if (retries-- == 0)
-			return 0;
-
-		_delay_ms(2);
-	} while (!READ(data->inputReg, data->pin));
-
-	SET_MODE(data->modeReg, data->pin, OUTPUT);
-	WRITE_LOW(data->outputReg, data->pin);         // pull low for 500uS
-	_delay_ms(500);
-	SET_MODE(data->modeReg, data->pin, INPUT);
-	WRITE_HIGH(data->outputReg, data->pin);        //enable pullup
-
-	_delay_ms(65);
-	r = READ(data->inputReg, data->pin);
-	_delay_ms(490);
-
-	return r;
-}
-
-//
-// Write a bit. data->port and bit is used to cut lookup time and provide
-// more certain timing.
-//
-void write_bit(uint8_t v, OneWire* data) {
-	static uint8_t lowTime[]  = { 55, 5 };
-	static uint8_t highTime[] = { 5, 55 };
-
-	v = (v & 1);
-	*(data->modeReg) |= data->bitmask;    // make data->pin an output, do first
-	                                      // since we expect to be at 1
-	*(data->outputReg) &= ~data->bitmask; // zero
-	_delay_ms(lowTime[v]);
-	*(data->outputReg) |= data->bitmask;  // one, push data->pin up - important for
-	                                      // parasites, they might start in here
-	_delay_ms(highTime[v]);
-}
-
-//
 // Read a bit. data->port and bit is used to cut lookup time and provide
 // more certain timing.
 //
@@ -152,25 +100,6 @@ uint8_t read_bit(OneWire* data) {
 }
 
 //
-// Write a byte. The writing code uses the active drivers to raise the
-// data->pin high, if you need power after the write (e.g. DS18S20 in
-// parasite power mode) then set 'power' to 1, otherwise the data->pin will
-// go tri-state at the end of the write to avoid heating in a short or
-// other mishap.
-//
-void write(uint8_t v, uint8_t power, OneWire* data) {
-	uint8_t bitmask;
-
-	for (bitmask = 0x01; bitmask; bitmask <<= 1) {
-		write_bit( (bitmask & v) ? 1 : 0, data);
-	}
-	if (!power) {
-		SET_MODE(data->modeReg, data->pin, INPUT);
-		WRITE_HIGH(data->outputReg, data->pin);
-	}
-}
-
-//
 // Read a byte
 //
 uint8_t read(OneWire* data) {
@@ -183,30 +112,6 @@ uint8_t read(OneWire* data) {
 	}
 
 	return r;
-}
-
-//
-// Do a ROM select
-//
-void select(uint8_t rom[8], OneWire* data) {
-	int i;
-
-	write(0x55 , 0, data);         // Choose ROM
-
-	for (i = 0; i < 8; i++) {
-		write(rom[i], 0, data);
-	}
-}
-
-//
-// Do a ROM skip
-//
-void skip(OneWire* data) {
-	write(0xCC, 0, data);         // Skip ROM
-}
-
-void depower(OneWire* data) {
-	SET_MODE(data->modeReg, data->pin, INPUT);
 }
 
 //
