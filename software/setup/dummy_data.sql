a44620d73e40350aa56cc75097cf39f2eb899e59
Alex Crawford <adrofwarc@gmail.com>
2011-09-05 22:33:10 -0400

    Updated dummy data

--- software/setup/dummy_data.sql
+++ software/setup/dummy_data.sql
@@ -7,7 +7,7 @@ INSERT INTO `denials` VALUES (1,2,2,NULL,NULL),(2,1,2,NULL,NULL);
 UNLOCK TABLES;
 
 LOCK TABLES `doors` WRITE;
-INSERT INTO `doors` VALUES (1,'z123',1,'Door 1',1,NULL),(2,'z124',1,'Door 2',1,NULL),(3,'e321',2,'Door 3',2,NULL);
+INSERT INTO `doors` VALUES (1,'z123',1,'Door 1',NULL),(2,'z124',1,'Door 2',NULL),(3,'e321',2,'Door 3',NULL);
 UNLOCK TABLES;
 
 LOCK TABLES `interfaces` WRITE;
