28a03c33bd97cbe376aa0be0b244ca0e5dc3f9a8
Alex Crawford <adrofwarc@gmail.com>
2011-09-21 19:41:48 -0400

    I'm dumb. Added super to site

--- software/site/test.rb
+++ software/site/test.rb
@@ -13,8 +13,11 @@ require 'ldap'
 require 'user'
 
 class Test < Sinatra::Base
-	@database = keys_to_symbols(YAML.load_file('config/database.yml')).freeze
-	@ldap     = keys_to_symbols(YAML.load_file('config/ldap.yml')).freeze
+	def initialize
+		@db_config   = keys_to_symbols(YAML.load_file('config/database.yml')).freeze
+		@ldap_config = keys_to_symbols(YAML.load_file('config/ldap.yml')).freeze
+		super
+	end
 
 	FETCH_LOG = '
 		SELECT users.uuid, types.name as type, actions.name as action, events.datetime
@@ -31,7 +34,7 @@ class Test < Sinatra::Base
 		WHERE id = %d
 	'.freeze
 
-	get '/' do 
+	get '/' do
 		hostname = Socket.gethostbyname(Socket.gethostname).first
 		#TODO - Look this up
 		wsport = 8080
@@ -39,8 +42,8 @@ class Test < Sinatra::Base
 	end
 
 	get '/log/:door' do
-		db = Mysql2::Client.new(@database)
-		ldap = Gatekeeper::Ldap.new(@ldap)
+		db = Mysql2::Client.new(@db_config)
+		ldap = Gatekeeper::Ldap.new(@ldap_config)
 
 		door = db.query(FETCH_DOOR_NAME % params[:door].to_i).first
 		log = db.query(FETCH_LOG % door['message_address'].to_i).collect do |entry|
